0cd098172fb640cb54e02f2cd10b7b6316f17ef9
Abhinav <abhinavmiriyala3@gmail.com>
2025-08-19 23:17:38 +0530

    grid

---
+++ Backend/Core java/operators.java
@@ -0,0 +1,36 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(b/a);
+        System.out.println(a%b);
+        System.out.println(a*b);
+        System.out.println(a++);
+        System.out.println(b++);
+
+        /*
+         * operators
+         * Arithemetic operator: the operators which is use to perform mathematical operations 
+         * eg. +,-,*,/,*,increment ++, decrement --
+         * 
+         * Assignment operator (=): the operators use to assign values to variables 
+         * eg. =, += , -=, *=, /=, %=
+         * 
+         * logical operators : it is use to perform boolean operands
+         * eg. AND(&&) -> the both conditions are true then it will return true 
+         *             -> if one the condition is false then it will return false
+         *     OR(||) -> it will return if one of its condition are true
+         *     NOT(!) -> it will reverse the condition if it returns true then it will convert it into false
+         * 
+         * Relational operators : use to compare two operands 
+         * eg: ==, !=, >, <, >=, <=
+         * 
+         * conditional operator: 
+        */
+
+        
+    }
+}
+
